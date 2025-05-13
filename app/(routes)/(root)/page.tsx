bfa22eb6835c9c1b765959d5df76aa9fc9219595
Matias <lit.io30303@gmail.com>
2025-05-13 19:23:47 -0400

    conf inicial

---
+++ app/(routes)/(root)/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import { Header } from "./components/Header";
+
+export default function Home() {
+  return (
+    <div>
+      <Header></Header>
+    </div>
+    
+  );
+}
